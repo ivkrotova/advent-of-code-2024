a8a1fcc4bc2edbb7aee4cebb58a00b4c7701ea50
Irina Krotova <i.v.krotova@gmail.com>
2024-12-04 14:00:32 +0100

    Remove prints

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -8,12 +8,10 @@ pub fn solve() -> io::Result<()> {
     let mut file = File::open(file_path).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    println!("File contents: {}", contents);
 
     let mut column1: Vec<i32> = Vec::new();
     let mut column2: Vec<i32> = Vec::new();
 
-    // Process each line of the file
     for line in contents.lines() {
         let mut parts = line.split_whitespace();
             if let (Some(col1), Some(col2)) = (parts.next(), parts.next()) {
@@ -24,16 +22,13 @@ pub fn solve() -> io::Result<()> {
                 }
             }  
 
-    println!("Column 1: {:?}", column1);
-    println!("Column 2: {:?}", column2);
     column1.sort();
-    println!("Column 1 sorted: {:?}", column1);
     column2.sort();
-    println!("Column 2 sorted: {:?}", column2);
+
     let total_difference: i32 = column1.iter()
         .zip(column2.iter())
-        .map(|(c1, c2)| (c1 - c2).abs()) // Compute absolute difference
-        .sum(); // Sum up the differences
+        .map(|(c1, c2)| (c1 - c2).abs())
+        .sum();
     println!("Total difference: {}", total_difference);
     
     Ok(())
